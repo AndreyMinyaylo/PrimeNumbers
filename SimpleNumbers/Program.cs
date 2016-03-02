eb6ebdefdcbbd62b962f9f2bfa2e8ae7a1028ca2
AndreyMinyaylo <minyailoandrey@gmail.com>
2016-03-02 11:28:39 +0200

    Rename_sourse

---
+++ SimpleNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SimpleNumbers
+{
+ 
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+             Console.WriteLine("Enter first number:");
+             var firstNumber = Convert.ToInt32(Console.ReadLine());
+
+             Console.WriteLine("Enter second number:");
+             var secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (firstNumber == 1)
+            {
+                firstNumber = firstNumber  + 1;
+            }
+
+            for (int i = firstNumber; i <= secondNumber; i++)
+            {
+                
+                int cnt = 0;
+                for (int j = 2; j <= i / 2; ++j)
+                {
+                    if (i % j == 0)
+                        cnt = 1;
+                }
+                if (cnt == 0)
+                    Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
