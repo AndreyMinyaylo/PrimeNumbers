50406ec909299ee6690c17caea6de0ead0f3527c
AndreyMinyaylo <minyailoandrey@gmail.com>
2016-04-07 19:59:06 +0300

    Test_oneNumber

---
+++ Test_one_SimpleNumber/UnitTest1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_one_SimpleNumber
+{
+    [TestClass]
+    public class Test_oneNumber
+    {
+        [TestMethod]
+        
+            public static bool isSimple(int number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+            bool is_simple = true;
+            for (int i = 2; i < number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    is_simple = false;
+                    break;
+                }
+            }
+            return is_simple;
+        }
+
+
+        public static void printSimleNumberEnd()
+        {
+            var st = "Number 7 is simple";
+            var sr = string.Empty;
+            var number = 7;
+            while (true)
+            {
+                Console.WriteLine('\n' + "Enter number and we check number is simple or no or enter 3 to exit:");
+
+                if (number == 3)
+                {
+                    break;
+                }
+                bool is_Simple = isSimple(number);
+                if (is_Simple == true)
+                {
+                    sr = sr +"Number " + number + " is simple";
+                }
+                else
+                {
+                    sr = sr + "Number " + number + " is not simple";
+                }
+                Assert.AreEqual(st, sr);
+            }
+        }
+
+
+        public static void Main(string[] args)
+            {
+                var number = 7;
+
+            Console.ReadKey();
+        }
+    }
+}
